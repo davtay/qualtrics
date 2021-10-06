012e8e78f9c31bea7bfe623a5a421672ed469422
davtay <davtay@live.com>
2021-10-06 07:48:21 -0700

    Initial creation

---
+++ qualtrics_contact_delete.py
@@ -0,0 +1,68 @@
+import requests, json, os, csv, sys
+from dotenv import load_dotenv
+
+def post_to_chat(status):
+    
+    webhook_url = os.getenv("WEBHOOK_URL")
+    message_headers = {'Content-Type' : 'application/json; charset=UTF-8'}
+    message_success = {'text' : 'Disabled staff deletion completed'}
+    message_errh = {'text' : 'Staff deletion - Failure: Http error'}
+    message_errc = {'text' : 'Staff deletion - Failure: Connection error'}
+    message_errt = {'text' : 'Staff deletion - Failure: Timeout'}
+    message_err = {'text' : 'Staff deletion - Failure: Something went wrong'}
+    
+    if status == 'success':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_success),headers=message_headers)
+    elif status == 'errh':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_errh),headers=message_headers)
+    elif status == 'errc':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_errc),headers=message_headers)
+    elif status == 'errt':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_errt),headers=message_headers)
+    else:
+        chat_message = requests.post(webhook_url,data=json.dumps(message_err),headers=message_headers)
+
+directory = os.getenv("QUALTRICS_DIRECTORY")
+qualtrics_url = os.getenv("QUALTRICS_URL")
+search_url = f"{qualtrics_url}{directory}/contacts/search"
+
+qualtrics_token = os.getenv("QUALTRICS_TOKEN")
+headers = {
+    "Content-Type": "application/json",
+    "X-API-TOKEN": qualtrics_token
+}
+
+qualtrics_file_loc = os.getenv("QUALTRICS_FILE_LOC")
+with open(qualtrics_file_loc, 'r') as csv_file:
+    field = next(csv_file)
+    while True:
+        email = csv_file.readline()
+        if not email:
+            break
+        payload = {"filter": {
+            "comparison": "eq",
+            "filterType": "email",
+            "value": email.strip()
+            }
+        }
+        response = requests.request("POST", search_url, json=payload, headers=headers)
+        contact = response.json()
+        if contact['result']['elements']:
+            contact_id = contact['result']['elements'][0]['id']
+            delete_contact_url = f"{qualtrics_url}{directory}/contacts/{contact_id}"
+            try:
+                delete_contact = requests.request("DELETE",delete_contact_url,headers=headers)
+                delete_contact.raise_for_status()
+                post_to_chat('success')
+            except requests.exceptions.HTTPError as errh:
+                post_to_chat('errh')
+                sys.exit()
+            except requests.exceptions.ConnectionError as errc:
+                post_to_chat('errc')
+                sys.exit()
+            except requests.exceptions.Timeout as errt:
+                post_to_chat('errt')
+                sys.exit()
+            except requests.exceptions.RequestException as err:
+                post_to_chat()
+                sys.exit()
---
+++ qualtrics_contact_upload.py
@@ -0,0 +1,44 @@
+import requests, json, os
+from dotenv import load_dotenv
+
+def post_to_chat(status):
+    
+    webhook_url = os.getenv("WEBHOOK_URL")
+    message_headers = {'Content-Type' : 'application/json; charset=UTF-8'}
+    
+    message_success = {'text' : 'Staff creation - Success'}
+    message_errh = {'text' : 'Staff creation - Failure: Http error'}
+    message_errc = {'text' : 'Staff creation - Failure: Connection error'}
+    message_errt = {'text' : 'Staff creation - Failure: Timeout'}
+    message_err = {'text' : 'Staff creation - Failure: Something went wrong'}
+    
+    if status == 'success':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_success),headers=message_headers)
+    elif status == 'errh':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_errh),headers=message_headers)
+    elif status == 'errc':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_errc),headers=message_headers)
+    elif status == 'errt':
+        chat_message = requests.post(webhook_url,data=json.dumps(message_errt),headers=message_headers)
+    else:
+        chat_message = requests.post(webhook_url,data=json.dumps(message_err),headers=message_headers)
+
+qualtrics_url = os.getenv("QUALTRICS_URL")
+qualtrics_token = os.getenv("QUALTRICS_TOKEN")
+qualtrics_file_name = os.getenv("QUALTRICS_FILE_NAME")
+qualtrics_file_loc = os.getenv("QUALTRICS_FILE_LOC")
+qualtrics_file = {'file':(qualtrics_file_name,open(qualtrics_file_loc,'rb'),'text/csv')}
+auth_header = {'x-api-token' : qualtrics_token}
+
+try:
+    upload_contacts = requests.post(qualtrics_url,headers=auth_header,files=qualtrics_file)
+    upload_contacts.raise_for_status()
+    post_to_chat('success')
+except requests.exceptions.HTTPError as errh:
+    post_to_chat('errh')
+except requests.exceptions.ConnectionError as errc:
+    post_to_chat('errc')
+except requests.exceptions.Timeout as errt:
+    post_to_chat('errt')
+except requests.exceptions.RequestException as err:
+    post_to_chat()
