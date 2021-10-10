7893c312b73e938afc563c404a49523c228a39bc
davtay <davtay@live.com>
2021-10-09 20:47:31 -0700

    Add boilerplate
    Updated to use if __name__ == '__main__' to act as entry point.

--- qualtrics_contact_delete.py
+++ qualtrics_contact_delete.py
@@ -1,6 +1,53 @@
 import requests, json, os, csv, sys
 from dotenv import load_dotenv
 
+def qualtrics_lookup_delete():
+    directory = os.getenv("QUALTRICS_DIRECTORY")
+    qualtrics_url = os.getenv("QUALTRICS_URL")
+    search_url = f"{qualtrics_url}{directory}/contacts/search"
+
+    qualtrics_token = os.getenv("QUALTRICS_TOKEN")
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-TOKEN": qualtrics_token
+    }
+
+    qualtrics_file_loc = os.getenv("QUALTRICS_FILE_LOC")
+    with open(qualtrics_file_loc, 'r') as csv_file:
+        field = next(csv_file)
+        while True:
+            email = csv_file.readline()
+            if not email:
+                break
+            payload = {"filter": {
+                "comparison": "eq",
+                "filterType": "email",
+                "value": email.strip()
+                }
+            }
+            response = requests.request("POST", search_url, json=payload, headers=headers)
+            contact = response.json()
+
+            if contact['result']['elements']:
+                contact_id = contact['result']['elements'][0]['id']
+                delete_contact_url = f"{qualtrics_url}{directory}/contacts/{contact_id}"
+                try:
+                    delete_contact = requests.request("DELETE",delete_contact_url,headers=headers)
+                    delete_contact.raise_for_status()
+                    post_to_chat('success')
+                except requests.exceptions.HTTPError as errh:
+                    post_to_chat('errh')
+                    sys.exit()
+                except requests.exceptions.ConnectionError as errc:
+                    post_to_chat('errc')
+                    sys.exit()
+                except requests.exceptions.Timeout as errt:
+                    post_to_chat('errt')
+                    sys.exit()
+                except requests.exceptions.RequestException as err:
+                    post_to_chat()
+                    sys.exit()
+
 def post_to_chat(status):
     
     webhook_url = os.getenv("WEBHOOK_URL")
@@ -21,48 +68,6 @@ def post_to_chat(status):
         chat_message = requests.post(webhook_url,data=json.dumps(message_errt),headers=message_headers)
     else:
         chat_message = requests.post(webhook_url,data=json.dumps(message_err),headers=message_headers)
-
-directory = os.getenv("QUALTRICS_DIRECTORY")
-qualtrics_url = os.getenv("QUALTRICS_URL")
-search_url = f"{qualtrics_url}{directory}/contacts/search"
-
-qualtrics_token = os.getenv("QUALTRICS_TOKEN")
-headers = {
-    "Content-Type": "application/json",
-    "X-API-TOKEN": qualtrics_token
-}
-
-qualtrics_file_loc = os.getenv("QUALTRICS_FILE_LOC")
-with open(qualtrics_file_loc, 'r') as csv_file:
-    field = next(csv_file)
-    while True:
-        email = csv_file.readline()
-        if not email:
-            break
-        payload = {"filter": {
-            "comparison": "eq",
-            "filterType": "email",
-            "value": email.strip()
-            }
-        }
-        response = requests.request("POST", search_url, json=payload, headers=headers)
-        contact = response.json()
-        if contact['result']['elements']:
-            contact_id = contact['result']['elements'][0]['id']
-            delete_contact_url = f"{qualtrics_url}{directory}/contacts/{contact_id}"
-            try:
-                delete_contact = requests.request("DELETE",delete_contact_url,headers=headers)
-                delete_contact.raise_for_status()
-                post_to_chat('success')
-            except requests.exceptions.HTTPError as errh:
-                post_to_chat('errh')
-                sys.exit()
-            except requests.exceptions.ConnectionError as errc:
-                post_to_chat('errc')
-                sys.exit()
-            except requests.exceptions.Timeout as errt:
-                post_to_chat('errt')
-                sys.exit()
-            except requests.exceptions.RequestException as err:
-                post_to_chat()
-                sys.exit()
+                
+if __name__ == '__main__':
+    qualtrics_lookup_delete()
